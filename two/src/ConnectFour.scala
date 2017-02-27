7931334bfd67cd0a9fef56e539a82be06f863594
Rick Boss <rickeboss@gmail.com>
2017-02-26 18:41:04 -0800

    Diagonal checking now too

--- two/src/ConnectFour.scala
+++ two/src/ConnectFour.scala
@@ -40,7 +40,130 @@ class ConnectBoard(w: Int, h: Int, dim: Int = 4) {
     val list = new java.util.ArrayList[Integer](w * h)
     (0 until w * h).foreach(_ => list.add(0))
 
+    var lastMoveIndex = Option.empty[Int]
     var lastPlayer = 2
+
+    def indexToXY(idx: Int): (Int, Int) = (idx % w, idx / w)
+    def xyToIndex(xy: (Int, Int)): Int = xy._2 * w + xy._1
+
+    def victor: Int = lastMoveIndex match {
+        case Some(last) =>
+            var l = true
+            var r = true
+            var u = true
+            var d = true
+            var b = true
+            var f = true
+            var bb = true
+            var ff = true
+
+            var sum_r = 1
+            var sum_c = 1
+            var sum_d = 1
+
+            for (i <- 1 to 3 if sum_r < dim & sum_c < dim & sum_d < dim) {
+                val xy = indexToXY(last)
+                if (l) {
+                    if (xy._1 - i >= 0) {
+                        val v = list.get(xyToIndex(xy._1 - i, xy._2))
+                        if (v == lastPlayer) {
+                            sum_r += 1
+                        } else {
+                            l = false
+                        }
+                    } else {
+                        l = false
+                    }
+                }
+                if (r) {
+                    if (xy._1 + i < w) {
+                        val v = list.get(xyToIndex(xy._1 + i, xy._2))
+                        if (v == lastPlayer) {
+                            sum_r += 1
+                        } else {
+                            r = false
+                        }
+                    } else {
+                        r = false
+                    }
+                }
+                if (u) {
+                    val xy2 = xyToIndex(xy._1, xy._2 - i)
+                    if (xy2 >= 0 && list.get(xy2) == lastPlayer) {
+                        sum_c += 1
+                    } else {
+                        u = false
+                    }
+                }
+                if (d) {
+                    val xy2 = xyToIndex(xy._1, xy._2 + i)
+                    if (xy2 < w * h && list.get(xy2) == lastPlayer) {
+                        sum_c += 1
+                    } else {
+                        d = false
+                    }
+                }
+                if (b) {
+                    if (xy._1 - i >= 0 && xy._2 >= 0) {
+                        if (list.get(xyToIndex(xy._1 - i, xy._2 - i)) == lastPlayer) {
+                            sum_d += 1
+                        } else {
+                            b = false
+                        }
+                    } else {
+                        b = false
+                    }
+                }
+                if (f) {
+                    if (xy._1 + i < w && xy._2 + i < h) {
+                        if (list.get(xyToIndex(xy._1 + i, xy._2 + i)) == lastPlayer) {
+                            sum_d += 1
+                        } else {
+                            f = false
+                        }
+                    } else {
+                        f = false
+                    }
+                }
+                if (bb) {
+                    if (xy._1 - i >= 0 && xy._2 + i < h) {
+                        if (list.get(xyToIndex(xy._1 - i, xy._2 + i)) == lastPlayer) {
+                            sum_d += 1
+                        } else {
+                            bb = false
+                        }
+                    } else {
+                        bb = false
+                    }
+                }
+                if (ff) {
+                    if (xy._1 + i < w && xy._2 - i >= 0) {
+                        if (list.get(xyToIndex(xy._1 + i, xy._2 - i)) == lastPlayer) {
+                            sum_d += 1
+                        } else {
+                            ff = false
+                        }
+                    } else {
+                        ff = false
+                    }
+                }
+            }
+
+            if (sum_r >= 4) {
+                println("Row")
+                lastPlayer
+            } else if (sum_c >= 4) {
+                println("Column")
+                lastPlayer
+            } else if (sum_d >= 4) {
+                println("Diagonal")
+                lastPlayer
+            } else {
+                0
+            }
+        case None =>
+            0
+    }
 
     def checkRows(list: List[Integer]): Int =  {
         list.sliding(w, w).foldLeft(0) { case (winner, row) =>
@@ -94,9 +217,11 @@ class ConnectBoard(w: Int, h: Int, dim: Int = 4) {
                 }
             }
         }
+
+        0
     }
 
-    def victor: Int = {
+    def victor_old: Int = {
         val scalaList = list.asScala.toList
         val rw = checkRows(scalaList)
 
@@ -131,42 +256,9 @@ class ConnectBoard(w: Int, h: Int, dim: Int = 4) {
         }
     }
 
-    def victor_old: Int = {
-        var w = 0
-        var n = 0
-        var l = list.get(0)
-        list.forEach { i: Integer =>
-            i.toInt match {
-                case _ if w != 0 => ;
-                case 0 => ;
-                case 1 =>
-                    if (l == 1) {
-                        n += 1
-                        if (n == dim) {
-                            w = 1
-                        }
-                    } else {
-                        n = 1
-                        l = 1
-                    }
-                case 2 =>
-                    if (l == 2) {
-                        n += 1
-                        if (n == dim) {
-                            w = 2
-                        }
-                    } else {
-                        n = 1
-                        l = 2
-                    }
-            }
-        }
-
-        w
-    }
-
     def place(row: Int, player: Int): Boolean = {
         if (list.get(row) != 0) {
+            lastMoveIndex = None
             false
         } else {
             var at = row
@@ -176,6 +268,8 @@ class ConnectBoard(w: Int, h: Int, dim: Int = 4) {
             }
             list.set(at, player)
             lastPlayer = player
+
+            lastMoveIndex = Option(at)
 
             true
         }
